b5ab0b5b4d9a5eca95202015aad206bc391f1cd3
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-08 23:49:33 +0100

    Hola mundo

---
+++ src/main/java/org/example/HolaMundo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class HolaMundo {
+
+    //Atajo psvm + tab
+    public static void main(String[] args) {
+
+        //Atajo sout + tab
+        System.out.println("Hola mundo");
+
+    }
+}
