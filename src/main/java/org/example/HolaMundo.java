268c9a0f1cc0a6c6102b5ed4c6ad38f2e0f85387
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-09 23:44:57 +0100

    Hola mundo sin hardcodear, usando una variable tipo String

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -5,8 +5,19 @@ public class HolaMundo {
     //Atajo psvm + tab
     public static void main(String[] args) {
 
+        //Esto es hardcodear
         //Atajo sout + tab
         System.out.println("Hola mundo");
+
+        //Así serían las buenas prácticas
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+
+        //Otro atajo es soutv que además trae la primera variable que encuentra
+        System.out.println("saludar = " + saludar);
+
+        //Podemos usar métodos predefinidos con saludar al ser un tipo String
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
 
     }
 }
