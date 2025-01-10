812bc51a3c745f5d9f0fcc98c64b0b92f0b2911f
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-10 16:15:06 +0100

    Primitivos tipo byte

---
+++ src/main/java/org/example/Primitivos.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Primitivos {
+    public static void main(String[] args) {
+
+        byte numeroByte = 127;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("tipo byte corresponde en byte a " + Byte.BYTES);
+        System.out.println("tipo byte corresponde en bites a " + Byte.SIZE);
+        System.out.println("valor máximo de un byte: " + Byte.MAX_VALUE);
+        System.out.println("valor mínimo de un byte: " + Byte.MIN_VALUE);
+    }
+}
