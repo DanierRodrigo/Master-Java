e4de44a81c38b591b94dc42ec6cf721eefa22589
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-10 17:57:04 +0100

    Primitivos tipo short

--- src/main/java/org/example/Primitivos.java
+++ src/main/java/org/example/Primitivos.java
@@ -9,5 +9,14 @@ public class Primitivos {
         System.out.println("tipo byte corresponde en bites a " + Byte.SIZE);
         System.out.println("valor máximo de un byte: " + Byte.MAX_VALUE);
         System.out.println("valor mínimo de un byte: " + Byte.MIN_VALUE);
+
+        System.out.println();
+
+        short numeroShort = 30000;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("tipo short corresponde en byte a " + Short.BYTES);
+        System.out.println("tipo short corresponde en bites a " + Short.SIZE);
+        System.out.println("valor máximo de un short: " + Short.MAX_VALUE);
+        System.out.println("valor mínimo de un short: " + Short.MIN_VALUE);
     }
 }
