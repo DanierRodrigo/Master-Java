85650e61a036c23c492d181d5aaa853043d789fc
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-11 15:27:36 +0100

    Primitivos reales dinámicos tipo var + correciones

--- src/main/java/org/example/PrimitivosFloat.java
+++ src/main/java/org/example/PrimitivosFloat.java
@@ -12,12 +12,18 @@ public class PrimitivosFloat {
 
         System.out.println();
 
-        float realDouble = 1;
+        double realDouble = 3.56354E32;
         System.out.println("realDouble = " + realDouble);
         System.out.println("tipo double corresponde en byte a " + Double.BYTES);
         System.out.println("tipo double corresponde en bites a " + Double.SIZE);
         System.out.println("valor máximo de un double: " + Double.MAX_VALUE);
         System.out.println("valor mínimo de un double: " + Double.MIN_VALUE);
+
+        System.out.println();
+        //Para variables dinámicas. Por defecto será Float
+        //Si quieremos más que float o distinto, lo terminaremos en L,F o D
+        var varFlotante = 127.423;
+        System.out.println("varFlotante = " + varFlotante);
     }
     
 }
