b41f13b5ee612bc6dd7b17c168484066fca1d4e5
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-11 15:15:57 +0100

    Primitivos tipo Double

--- src/main/java/org/example/PrimitivosFloat.java
+++ src/main/java/org/example/PrimitivosFloat.java
@@ -9,6 +9,15 @@ public class PrimitivosFloat {
         System.out.println("tipo float corresponde en bites a " + Float.SIZE);
         System.out.println("valor máximo de un float: " + Float.MAX_VALUE);
         System.out.println("valor mínimo de un float: " + Float.MIN_VALUE);
+
+        System.out.println();
+
+        float realDouble = 1;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("tipo double corresponde en byte a " + Double.BYTES);
+        System.out.println("tipo double corresponde en bites a " + Double.SIZE);
+        System.out.println("valor máximo de un double: " + Double.MAX_VALUE);
+        System.out.println("valor mínimo de un double: " + Double.MIN_VALUE);
     }
     
 }
