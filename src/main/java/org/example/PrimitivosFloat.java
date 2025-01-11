552d28afb16a3ac2eb22afa301e312d956271934
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-11 15:06:35 +0100

    Primitivos tipo Float

--- src/main/java/org/example/PrimitivosFloat.java
+++ src/main/java/org/example/PrimitivosFloat.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class PrimitivosFloat {
+    public static void main(String[] args) {
+        
+        float realFloat = 1;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("tipo float corresponde en byte a " + Float.BYTES);
+        System.out.println("tipo float corresponde en bites a " + Float.SIZE);
+        System.out.println("valor máximo de un float: " + Float.MAX_VALUE);
+        System.out.println("valor mínimo de un float: " + Float.MIN_VALUE);
+    }
+    
 }
