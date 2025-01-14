4cd214fcb466775d13f2554d91ea202d36f8a3e7
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-14 23:43:18 +0100

    Decimales, Binarios, Octales y Hexadecimales.

--- src/main/java/org/example/SistemasNumericos.java
+++ src/main/java/org/example/SistemasNumericos.java
@@ -1,4 +1,37 @@
 package org.example;
 
 public class SistemasNumericos {
+    public static void main(String[] args) {
+
+        int numeroDecimal = 500;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        System.out.println();
+
+        System.out.println("numero binario de  " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+
+        System.out.println();
+
+        int numeroBinario = 0b111110100;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        System.out.println();
+
+        System.out.println("numero octal de " + numeroBinario + " = " + Integer.toOctalString(numeroDecimal));
+
+        System.out.println();
+
+        int numeroOctal = 0764;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        System.out.println();
+
+        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+
+        System.out.println();
+
+        int numeroHex = 0x1F4;
+        System.out.println("numeroHex = " + numeroHex);
+
+    }
 }
