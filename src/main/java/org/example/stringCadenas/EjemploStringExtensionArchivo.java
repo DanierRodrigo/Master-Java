31617d925033fa37b59520df438de4ea02f0793b
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-24 01:03:41 +0100

    Ejercicio Obtener extensión de un archivo usando métodos String

--- src/main/java/org/example/stringCadenas/EjemploStringExtensionArchivo.java
+++ src/main/java/org/example/stringCadenas/EjemploStringExtensionArchivo.java
@@ -1,4 +1,15 @@
 package org.example.stringCadenas;
 
 public class EjemploStringExtensionArchivo {
+    public static void main(String[] args) {
+
+        //Manera de obtener la extensión de archivos
+
+        String archivo = "alguna_imagen.jpeg";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("nombre.substring(i+1) = " + archivo.substring(i+1));
+    }
+
+
 }
