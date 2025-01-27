c1993f37cf21d7542599e74a8eaba6b2661f9c9f
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-27 13:29:49 +0100

    Tarea Numero Par o Impar

--- src/main/java/org/example/operadores/EjercicioNumeroParOImpar.java
+++ src/main/java/org/example/operadores/EjercicioNumeroParOImpar.java
@@ -1,4 +1,15 @@
 package org.example.operadores;
 
+import javax.swing.*;
+
 public class EjercicioNumeroParOImpar {
+    public static void main(String[] args) {
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número"));
+        if(numero % 2 == 0){
+            System.out.println("numero par = " + numero);
+        }else{
+            System.out.println("numero impar = " + numero);
+        }
+    }
 }
