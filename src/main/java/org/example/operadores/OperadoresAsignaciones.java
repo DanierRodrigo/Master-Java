060bb6240db84a8f1bfae05a85a582e7b173d114
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-28 23:38:54 +0100

    Operadores de asignación

--- src/main/java/org/example/operadores/OperadoresAsignaciones.java
+++ src/main/java/org/example/operadores/OperadoresAsignaciones.java
@@ -1,4 +1,29 @@
 package org.example.operadores;
 
 public class OperadoresAsignaciones {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int j = i + 4;
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+
+        i += 2; // i = i + 2
+        System.out.println("i += 2 = " + i);
+
+        i += 5; // i = i + 5
+        System.out.println("i = += 5 = " + i);
+
+        j -= 4; // j = j - 4
+        System.out.println("j -= 4 = " + j);
+
+        j*= 3; // j = j * 3;
+        System.out.println("j*= 3 = " + j);
+
+        String sqlString = "select * from clientes as c";
+        sqlString += " where c.nombre = 'Daniel' ";
+        sqlString += " and c.activo= 1 ";
+        System.out.println("sqlString = " + sqlString);
+    }
 }
