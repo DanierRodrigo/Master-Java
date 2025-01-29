d66b8bb616d833b0d4ee9b3de0e38dc22abb88b8
DanierRodrigo <danierrodrigo@gmail.com>
2025-01-29 23:41:32 +0100

    Operadores Unarios

--- src/main/java/org/example/operadores/OperadoresUnarios.java
+++ src/main/java/org/example/operadores/OperadoresUnarios.java
@@ -1,4 +1,23 @@
 package org.example.operadores;
 
 public class OperadoresUnarios {
+    public static void main(String[] args) {
+
+//Se usan realmente para cambiar el signo del entero a conveniencia
+
+        int i = -5;
+        int j = +i; // j = (i)*i => -5
+        System.out.println("j = +i = " + j);
+
+        int k = -i; // k = (i)*i => 5
+        System.out.println("k = -i = " + k);
+
+        i = 6;
+        j = +1;
+        System.out.println("j = +1 = " + j);
+
+        k = -1;
+        System.out.println("k = -1 = " + k);
+
+    }
 }
