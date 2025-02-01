7b7d5ebfc32f943513dc39388c49ba4b49e7756d
DanierRodrigo <danierrodrigo@gmail.com>
2025-02-01 18:20:06 +0100

    Ejercicio Login con Operadores Lógicos

--- src/main/java/org/example/operadores/EjercicioLoginConOperadoresLogicos.java
+++ src/main/java/org/example/operadores/EjercicioLoginConOperadoresLogicos.java
@@ -1,4 +1,33 @@
 package org.example.operadores;
 
+import java.util.Scanner;
+
 public class EjercicioLoginConOperadoresLogicos {
+    public static void main(String[] args) {
+
+        String username = "daniel";
+        String password = "12345";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre de usuario: ");
+        String u = scanner.next();
+
+        System.out.println("Ingrese la contraseña: ");
+        String p = scanner.next();
+
+        boolean esAutenticado = false;
+
+        if(username.equals(u) && password.equals(p)){
+            esAutenticado = true;
+        }else{
+            System.out.println("El nombre de usuario o contraseña son incorrectos");
+        }
+
+        if(esAutenticado){   //No es necesario poner = true porque ya se entiende de esta forma
+            System.out.println("Bienvenido usuario ".concat(u).concat("!"));
+        }else{
+            System.out.println("La autenticación es obligatoria");
+        }
+    }
 }
