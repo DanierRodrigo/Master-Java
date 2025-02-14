d4a4588ae1605c798175057b2ce3d9f86ded3676
drodrigor <danierrodrigo@gmail.com>
2025-02-14 13:23:15 +0100

    Ejercicio Buscar palabras dentro de una frase

--- src/main/java/org/example/flujosDeControl/SentenciasBucleEtiquetasBuscar.java
+++ src/main/java/org/example/flujosDeControl/SentenciasBucleEtiquetasBuscar.java
@@ -4,15 +4,21 @@ public class SentenciasBucleEtiquetasBuscar {
     public static void main(String[] args) {
 
         String frase = "tres tristes tigres tragan trigo en un trigal";
-        int max = frase.length();
+        String palabra = "trigo";
+        int maxFrase = frase.length();
+        int maxPalabra = palabra.length();
         int cantidad = 0;
-        char letra = 'e';
-        for (int i = 0; i < max; i++) {
-            if(frase.charAt(i) != letra){
-                continue;
+
+        buscar:
+        for (int i = 0; i < maxFrase; i++) {          //Este bucle recorre cada caracter de la frase
+            int k = i;
+            for (int j = 0; j < maxPalabra; j++) {    //Este bucle recorre cada caracter de la palabra
+                if(frase.charAt(k++) != palabra.charAt(j)){
+                    continue buscar;
+                }
             }
-            cantidad++;
+            cantidad++;      //Si todos los caracteres de la palabra coinciden con una sección de frase, se incrementa el contador cantidad
         }
-        System.out.println("Encontrado = " + cantidad + " veces el caracter '"+letra+"' en la frase: " + frase);
+        System.out.println("Encontrado = " + cantidad + " veces la palabra '"+palabra+"' en la frase: " + frase);
     }
 }
