6bf9ba83c4702ba9e33b2a829f387b55287e3daf
drodrigor <danierrodrigo@gmail.com>
2025-02-14 09:13:09 +0100

    Sentencia If e If Else

--- src/main/java/org/example/flujosDeControl/SentenciaIfElse.java
+++ src/main/java/org/example/flujosDeControl/SentenciaIfElse.java
@@ -1,4 +1,29 @@
 package org.example.flujosDeControl;
 
 public class SentenciaIfElse {
+    public static void main(String[] args) {
+
+        float promedio = 5.8f;
+
+        if (promedio >= 6.5) {                                                          //Si se cumple una expresión, por dentro se toma un booleano true
+            System.out.println("Felicidades, excelente promedio");               //Ocurrirá lo que se contemple aqui
+        } else {                                                                      //Al existir un else, cuando la expresión sea false
+            System.out.println("Necesitas esforzarte un poco más");               //Ocurrirá lo que se contemple aqui
+        }
+
+
+        float nota = 6.4f;
+
+        if (nota >= 6.5) {
+            System.out.println("Felicidades, excelente promedio");
+        } else if (nota >= 6.0) {
+            System.out.println("Muy buen promedio");
+        } else if (nota >= 5.5) {
+            System.out.println("Buen promedio");
+        } else if (nota >= 5.0) {
+            System.out.println("Regular, necesitas esforzarte un poco más");
+        } else {
+            System.out.println("Suspenso");
+        }
+    }
 }
