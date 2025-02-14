d87704ac1c3fa24bbf6126c91ca95d5a24a487c2
drodrigor <danierrodrigo@gmail.com>
2025-02-14 09:57:36 +0100

    Sentencia While

--- src/main/java/org/example/flujosDeControl/SentenciaWhile.java
+++ src/main/java/org/example/flujosDeControl/SentenciaWhile.java
@@ -1,4 +1,35 @@
 package org.example.flujosDeControl;
 
 public class SentenciaWhile {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while(i <= 5){       //Ejecuta un bloque mientras se cumple una condición
+            System.out.println("i = " + i);
+            i++;
+        }
+
+        i = 0;
+        boolean prueba = true;
+
+        while(prueba){
+            if(i == 7){
+                prueba = false;
+            }
+            System.out.println("i = " + i);
+            i++;
+        }
+
+        System.out.println();
+
+        prueba = false;
+        while (prueba){
+            System.out.println("nunca se ejecuta, porque prueba es false");
+        }
+
+        do {
+            System.out.println("Se ejecuta una vez al menos");
+        } while (prueba);
+
+    }
 }
