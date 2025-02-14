90b28ee87a224e6a38568f81eaf119042ce4e39f
drodrigor <danierrodrigo@gmail.com>
2025-02-14 09:30:32 +0100

    Sentencia Switch Case

--- src/main/java/org/example/flujosDeControl/SenteciaSwitchCase.java
+++ src/main/java/org/example/flujosDeControl/SenteciaSwitchCase.java
@@ -1,4 +1,50 @@
 package org.example.flujosDeControl;
 
+import java.util.Scanner;
+
 public class SenteciaSwitchCase {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número del 1 al 3: ");
+        int num = scanner.nextInt();
+
+        switch(num){
+            case 0:
+                System.out.println("El número es cero");
+                break;                                    //Sin break se contemplarían el resto de cases
+            case 1:
+                System.out.println("El número es uno");
+                break;
+            case 2:
+                System.out.println("El número es dos");
+                break;
+            case 3:
+                System.out.println("El número es tres");
+                break;
+            default:                                       //Break es como el else. Es lo que ocurre si nada es true
+                System.out.println("Número o caracter desconocido");
+        }
+
+        System.out.print("Ingrese un nombre: ");
+        String nombre = scanner.next();
+
+        switch(nombre){
+            case "admin":
+                System.out.println("Hola Admin, bienvenido");
+                break;
+            case "daniel":
+                System.out.println("Hola Daniel, bienvenido");
+                break;
+            case "july":
+                System.out.println("Hola July, bienvenida");
+                break;
+            case "alucard":
+                System.out.println("Hola Alucard, bienvenido");
+                break;
+            default:
+                System.out.println("Usuario desconocido");
+        }
+    }
 }
