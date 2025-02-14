a3a56ec952b4c4c9b5a4a65c91eb7ba3166284dd
drodrigor <danierrodrigo@gmail.com>
2025-02-14 11:03:56 +0100

    Ejercicio Buscar caracter dentro de una frase

--- src/main/java/org/example/flujosDeControl/SentenciasBucleEtiquetasBuscar.java
+++ src/main/java/org/example/flujosDeControl/SentenciasBucleEtiquetasBuscar.java
@@ -1,4 +1,18 @@
 package org.example.flujosDeControl;
 
 public class SentenciasBucleEtiquetasBuscar {
+    public static void main(String[] args) {
+
+        String frase = "tres tristes tigres tragan trigo en un trigal";
+        int max = frase.length();
+        int cantidad = 0;
+        char letra = 'e';
+        for (int i = 0; i < max; i++) {
+            if(frase.charAt(i) != letra){
+                continue;
+            }
+            cantidad++;
+        }
+        System.out.println("Encontrado = " + cantidad + " veces el caracter '"+letra+"' en la frase: " + frase);
+    }
 }
