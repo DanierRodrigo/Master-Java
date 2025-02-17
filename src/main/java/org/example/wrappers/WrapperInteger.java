9a41c1419b4d33c0194aad63bd7c177d7d7ad716
drodrigor <danierrodrigo@gmail.com>
2025-02-17 14:45:20 +0100

    Wrapper Integer

--- src/main/java/org/example/wrappers/WrapperInteger.java
+++ src/main/java/org/example/wrappers/WrapperInteger.java
@@ -1,4 +1,33 @@
 package org.example.wrappers;
 
 public class WrapperInteger {
+    public static void main(String[] args) {
+
+        int intPrimitivo = 78685;
+        Integer intAutoboxing = Integer.valueOf(intPrimitivo); //Forma autoboxing
+
+        Integer intObjeto = Integer.valueOf(intPrimitivo); //Forma explícita
+        Integer intObjeto1 = intPrimitivo;                    //Forma implícita
+
+        System.out.println("intObjeto = " + intObjeto);
+
+        int num = intObjeto;              //Forma implícita
+        int num2 = intObjeto.intValue();  //Forma explícita
+
+        System.out.println("num2 = " + num2);
+
+        String valorTv = "7467";
+        Integer valor = Integer.valueOf(valorTv);
+        System.out.println("valor = " + valor);
+
+        Short shortObjeto = intObjeto.shortValue();
+        System.out.println("shortObjeto = " + shortObjeto);
+        
+        Byte byteObjeto = intObjeto.byteValue();              //Se va a perder información
+        System.out.println("byteObjeto = " + byteObjeto);     //Es mejor convertir de más pequeño a más grande
+        
+        Long longObjeto = intObjeto.longValue();
+        System.out.println("longObjeto = " + longObjeto);
+        
+    }
 }
