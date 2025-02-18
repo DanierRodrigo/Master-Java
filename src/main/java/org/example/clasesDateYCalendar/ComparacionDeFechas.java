bfd9c16b35cf62648d702bb96206e555106db24b
drodrigor <danierrodrigo@gmail.com>
2025-02-18 18:22:01 +0100

    Comparación de fechas .equals

--- src/main/java/org/example/clasesDateYCalendar/ComparacionDeFechas.java
+++ src/main/java/org/example/clasesDateYCalendar/ComparacionDeFechas.java
@@ -16,7 +16,7 @@ public class ComparacionDeFechas {
             // Comparar las fechas usando before(), after() y equals()
             System.out.println("Fecha1 es antes que Fecha2? " + fecha1.before(fecha2));  // true
             System.out.println("Fecha1 es después que Fecha2? " + fecha1.after(fecha2));  // false
-
+            System.out.println("Fecha1 es igual a Fecha2? " + fecha1.equals(fecha2));     // false
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
