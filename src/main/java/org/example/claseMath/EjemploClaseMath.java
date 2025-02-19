f1a3a42689c9b880cc5ed25e55368b9bb1fd3c54
drodrigor <danierrodrigo@gmail.com>
2025-02-19 13:57:10 +0100

    Clase Math

--- src/main/java/org/example/claseMath/EjemploClaseMath.java
+++ src/main/java/org/example/claseMath/EjemploClaseMath.java
@@ -1,4 +1,29 @@
 package org.example.claseMath;
 
 public class EjemploClaseMath {
+    public static void main(String[] args) {
+
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto de -3 = " + absoluto);
+
+        absoluto = Math.abs(3);
+        System.out.println("absoluto de 3 = " + absoluto);
+
+        double max = Math.max(3.5, 1.2);
+        System.out.println("max entre 3.5 y 1.2 = " + max);
+
+        double min = Math.min(3.5, 1.2);
+        System.out.println("min entre 3.5 y 1.2 = " + min);
+
+        double redondeoArriba = Math.ceil(3.5);
+        System.out.println("redondeoArriba de 3.5 = " + redondeoArriba);
+
+        double redondeoAbajo = Math.floor(3.5);
+        System.out.println("redondeoAbajo de 3.5 = " + redondeoAbajo);
+
+        double redondeo = Math.round(Math.PI);
+        System.out.println("redondeo del número PI = " + redondeo);
+
+
+    }
 }
