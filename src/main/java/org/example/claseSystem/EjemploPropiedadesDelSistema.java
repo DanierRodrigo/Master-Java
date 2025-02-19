1929b48a2c5ebfbc3d4ab49e3736f338254105eb
drodrigor <danierrodrigo@gmail.com>
2025-02-19 12:06:26 +0100

    Sacar propiedades del sistema usando System

--- src/main/java/org/example/claseSystem/EjemploPropiedadesDelSistema.java
+++ src/main/java/org/example/claseSystem/EjemploPropiedadesDelSistema.java
@@ -1,4 +1,26 @@
 package org.example.claseSystem;
 
+import java.util.Properties;
+
 public class EjemploPropiedadesDelSistema {
+    public static void main(String[] args) {
+
+        String username = System.getProperty("user.name");
+        System.out.println("username = " + username);
+
+        String home = System.getProperty("user.home");
+        System.out.println("home = " + home);
+        
+        String workspace = System.getProperty("user.dir");
+        System.out.println("workspace = " + workspace);
+        
+        String java = System.getProperty("java.version");
+        System.out.println("java = " + java);
+
+        String lineSeparator = System.getProperty(System.lineSeparator());
+
+        Properties p = System.getProperties();
+        p.list(System.out);
+        
+    }
 }
