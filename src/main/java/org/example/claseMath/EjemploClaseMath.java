13dfbdde41ca6251df5ffebbd1d3cbf5b1a4cd62
drodrigor <danierrodrigo@gmail.com>
2025-02-19 14:49:13 +0100

    Clase Math trigonometría

--- src/main/java/org/example/claseMath/EjemploClaseMath.java
+++ src/main/java/org/example/claseMath/EjemploClaseMath.java
@@ -24,6 +24,36 @@ public class EjemploClaseMath {
         double redondeo = Math.round(Math.PI);
         System.out.println("redondeo del número PI = " + redondeo);
 
+        double exp = Math.exp(2);
+        System.out.println("exponente de 2 = " + exp);
+
+        double logaritmo = Math.log(10);
+        System.out.println("logaritmo de 10 = " + logaritmo);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println("potencia de 10 elevado a 3 = " + potencia);
+
+        double raiz = Math.sqrt(5);
+        System.out.println("raiz de 5 = " + raiz);
+
+
+        //Métodos de trigonometría
+
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);
+        System.out.println("convertir de radianes a grados = " + grados);
+
+        double radianes = Math.toRadians(90.00);
+        System.out.println("convertir de grados a radianes = " + radianes);
+
+        System.out.println("sin(90): " + Math.sin(radianes));
+        System.out.println("cos(90): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(180.00);
+        System.out.println("cos(180.00): = " + radianes);
+
+        radianes = Math.toRadians(0.00);
+        System.out.println("cos(0) = " + Math.cos(radianes));
 
     }
 }
