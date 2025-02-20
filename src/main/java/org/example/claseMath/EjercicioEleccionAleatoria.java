d580888e892d6bd40aba151cf700231b924ce56d
drodrigor <danierrodrigo@gmail.com>
2025-02-20 14:21:34 +0100

    Ejercicio Elección Aleatoria con cadena de String

--- src/main/java/org/example/claseMath/EjercicioEleccionAleatoria.java
+++ src/main/java/org/example/claseMath/EjercicioEleccionAleatoria.java
@@ -1,4 +1,27 @@
 package org.example.claseMath;
 
+import java.util.Random;
+
 public class EjercicioEleccionAleatoria {
+    public static void main(String[] args) {
+            // Arreglo con varias opciones
+            String[] opciones = {
+                    "Ir al cine",
+                    "Leer un libro",
+                    "Salir a correr",
+                    "Ver una serie",
+                    "Cocinar una receta nueva",
+                    "Jugar videojuegos"
+            };
+
+            // Crear un objeto Random para generar un número aleatorio
+            Random random = new Random();
+
+            // Elegir un índice aleatorio entre 0 y el tamaño del arreglo (opciones.length - 1)
+            int indiceAleatorio = random.nextInt(opciones.length);
+
+            // Mostrar la opción seleccionada
+            System.out.println("La opción seleccionada es: " + opciones[indiceAleatorio]);
+
+    }
 }
