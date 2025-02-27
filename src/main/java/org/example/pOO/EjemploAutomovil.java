dfe993fc158a370c2f7107ac28cb5647f68076d7
drodrigor <danierrodrigo@gmail.com>
2025-02-27 18:08:53 +0100

    Usando detalle()

--- src/main/java/org/example/pOO/EjemploAutomovil.java
+++ src/main/java/org/example/pOO/EjemploAutomovil.java
@@ -15,15 +15,9 @@ public class EjemploAutomovil {
         auto1.color = "Verde";
         auto1.cilindrada = 2.0;
 
-        System.out.println("auto.fabricante = " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrada = " + auto.cilindrada);
-
-        System.out.println("\nauto1.fabricante = " + auto1.fabricante);
-        System.out.println("auto1.modelo = " + auto1.modelo);
-        System.out.println("auto1.color = " + auto1.color);
-        System.out.println("auto1.cilindrada = " + auto1.cilindrada);
+        auto.detalle();
+        System.out.println();
+        auto1.detalle();
 
     }
 }
