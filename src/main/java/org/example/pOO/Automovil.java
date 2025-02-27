7973635c19260fbd44585d872342259264a7283f
drodrigor <danierrodrigo@gmail.com>
2025-02-27 18:08:15 +0100

    Método detalle()

--- src/main/java/org/example/pOO/Automovil.java
+++ src/main/java/org/example/pOO/Automovil.java
@@ -7,4 +7,10 @@ public class Automovil {
     String color;
     double cilindrada;
 
+    public void detalle(){
+        System.out.println("coche.fabricante = " + this.fabricante);
+        System.out.println("coche.modelo = " + this.modelo);
+        System.out.println("coche.color = " + this.color);
+        System.out.println("coche.cilindrada = " + this.cilindrada);
+    }
 }
