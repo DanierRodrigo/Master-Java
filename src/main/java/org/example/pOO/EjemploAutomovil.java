607ff777e2bf05cf030b75833b22573e491cde8b
drodrigor <danierrodrigo@gmail.com>
2025-03-03 13:53:19 +0100

    Usando .establecerDetalles() sobrecargado

--- src/main/java/org/example/pOO/EjemploAutomovil.java
+++ src/main/java/org/example/pOO/EjemploAutomovil.java
@@ -10,10 +10,15 @@ public class EjemploAutomovil {
         Automovil auto1 = new Automovil();
         auto1.establecerDetalles("Volkswagen", "Passat", "Verde", 2.0); // Usamos el metodo con argumentos
 
+        Automovil auto2 = new Automovil();
+        auto2.establecerDetalles("Toyota", "Corolla"); // Solo se establecen fabricante y modelo);
+
         // Mostrar los detalles de ambos automóviles
         System.out.println(auto.obtenerDetalles());
         System.out.println(); // Salto de línea
         System.out.println(auto1.obtenerDetalles());
+        System.out.println(); // Salto de línea
+        System.out.println(auto2.obtenerDetalles());
 
         // Modificar detalles con los métodos setters
         auto.setColor("Azul");
