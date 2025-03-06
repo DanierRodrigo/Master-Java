47001a57b13d98e996d92474ef370c76667eafe3
drodrigor <danierrodrigo@gmail.com>
2025-03-06 09:21:34 +0100

    Clase EjemploOrdenes creando clientes

---
+++ src/main/java/org/example/pOO/EjemploOrdenes.java
@@ -0,0 +1,14 @@
+package org.example.pOO;
+
+import java.util.Date;
+
+public class EjemploOrdenes {
+    public static void main(String[] args) {
+
+        // Crear clientes
+        Cliente cliente1 = new Cliente("Juan", "Pérez");
+        Cliente cliente2 = new Cliente("Ana", "Gómez");
+        Cliente cliente3 = new Cliente("Luis", "Martínez");
+
+    }
+}
