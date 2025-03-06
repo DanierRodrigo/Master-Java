116d9cfacf6894cee07f0263fe50289d15015d8d
drodrigor <danierrodrigo@gmail.com>
2025-03-06 17:53:52 +0100

    Clase abstracta Producto con atributos, constructor, getters y toString()

---
+++ src/main/java/org/example/pOO/herencias/AlmacenVerduras/Producto.java
@@ -0,0 +1,22 @@
+package org.example.pOO.herencias.AlmacenVerduras;
+
+abstract class Producto {
+    protected String nombre;
+    protected double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
